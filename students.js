655cab6417f0c1bb3b765001636235626b37fcde
tiwariuday70 <tiwariuday70@gmail.com>
2021-10-29 14:05:13 +0545

    initial commit

---
+++ students.js
@@ -0,0 +1,36 @@
+const students=[{
+    "id": 1,
+    "first_name": "Noland",
+    "last_name": "McLoughlin",
+    "email": "nmcloughlin0@creativecommons.org",
+    "gender": "Male"
+  }, {
+    "id": 2,
+    "first_name": "Padriac",
+    "last_name": "Dirr",
+    "email": "pdirr1@bandcamp.com",
+    "gender": "Bigender"
+  }, {
+    "id": 3,
+    "first_name": "Cinnamon",
+    "last_name": "Benda",
+    "email": "cbenda2@jugem.jp",
+    "gender": "Non-binary"
+  }, {
+    "id": 4,
+    "first_name": "Elvera",
+    "last_name": "Rey",
+    "email": "erey3@home.pl",
+    "gender": "Non-binary"
+  }, {
+    "id": 5,
+    "first_name": "Frannie",
+    "last_name": "Peppard",
+    "email": "fpeppard4@weather.com",
+    "gender": "Bigender"
+  }]
+
+
+
+
+  module.exports=  students
